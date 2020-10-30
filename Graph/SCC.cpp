f6217731385291cb0f5f53ff932d9cc569430a01
tsyr <33116430+tsyr@users.noreply.github.com>
2020-10-30 18:43:49 +0800

    Create SCC.cpp

---
+++ Graph/SCC.cpp
@@ -0,0 +1,39 @@
+int n, m;
+vector<int> v[maxn], rv[maxn]; //都要連!!
+int scc_cnt; //Number of scc 
+int used[maxn], sccno[maxn];
+vector<int> vs;
+
+void dfs1(int now){
+    used[now] = 1;
+    for (int i = 0; i < v[now].size(); i++) {
+        if (!used[v[now][i]])
+            dfs1(v[now][i]);
+    }
+    vs.push_back(now);
+}
+void dfs2(int now){
+    used[now] = 1;
+    sccno[now] = scc_cnt;
+    for (int i = 0; i < rv[now].size(); i++){
+        if (!used[rv[now][i]])
+            dfs2(rv[now][i]);
+    }
+}
+void find_scc(int nn){
+    memset(sccno, 0, sizeof(sccno));
+    scc_cnt = 0;
+    memset(used, 0, sizeof(used));
+    for (int i = 1; i <= nn; i++){ 
+        //Note that you may want to change the node range.
+        if (!used[i]) dfs1(i);
+    }
+    memset(used, 0, sizeof(used));
+    for (int i = vs.size() - 1; i >= 0; i--){
+        if (!used[vs[i]]){
+            scc_cnt++;
+            dfs2(vs[i]);
+        }
+    }
+    vs.clear();
+}
