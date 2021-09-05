1a23bf8640b6ec7a9c381d34798caa730ff46058
tsyr <33116430+tsyr@users.noreply.github.com>
2021-09-05 14:51:10 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,55 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define mod 1000000007
-#define ll long long 
+int main()
+{
+	int n;
+	cin>>n;
+	int ans=2e9;
+	for(int i=1 ; i<=sqrt(n) ; i++)
+	{
+		if(n%i==0)
+		{
 
-string n, m;
-
-ll a[55], b[55];
-
-     
-int main(){
-
-	cin >> n >> m;
-
-	int dif = m.size() - n.size();
-	int i = 0, j = 0;
-	int nn;
-	if (dif > 0){
-		i = dif;
-		nn = m.size();
-	}else{
-		j = dif;
-		nn = n.size();
-	}
-	int ii = 0, jj = 0;
-	for (; i< nn; ++ i){
-		a[i] = n[ii];
-		++ii;
-	}
-	for (; j< nn; ++ j){
-		b[j] = m[jj];
-		++jj;
-	}
-
-	ll ans = 0;
-	for (int k = 0; k < nn; ++k){
-		ll tot = a[k];
-		for (int kk = k + 1; kk < nn; ++kk){
-			tot = (tot * (10-b[kk]) % mod);
+			int a=i;
+			int b=n/i;
+			//cout<<a<<" "<<((int)(b+2)/2)*((int)(a+2)/2)<<"\n";
+			ans=min(ans,((b+2)/2)*((a+2)/2));
+			swap(a,b);
+			ans=min(ans,((b+2)/2)*((a+2)/2));
 		}
-
-
-		ll cnt  = 1;
-		for (int kk = k + 1; kk < nn; ++kk){
-			if (a[kk] - b[kk] >= 0){
-				cnt = (cnt + a[kk] - b[kk] + 1) % mod;
-			}else {
-				cnt = 0;
-				break;
-			}
-		}
-		ans = (ans + tot + cnt) % mod;
 	}
-	cout << ans << endl;
+	cout<<ans<<"\n";
 }
