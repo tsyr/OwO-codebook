57dbb5dd28ccdfeaa2de9480f0ba95d2d2233336
tsyr <33116430+tsyr@users.noreply.github.com>
2021-12-21 23:00:33 +0800

    Update 類歐幾里得算法.cpp

--- Math/類歐幾里得算法.cpp
+++ Math/類歐幾里得算法.cpp
@@ -1,4 +1,4 @@
-\sum_{i = 0}^{N - 1} floor((A \times i + B) / M).
+\sum_{i = 0}^{N} floor((A \times i + B) / M).
 ll f(int n, int a, int b, int c) {
 	if (!a) return (b / c) * (n + 1);
 	if (a >= c || b >= c)
